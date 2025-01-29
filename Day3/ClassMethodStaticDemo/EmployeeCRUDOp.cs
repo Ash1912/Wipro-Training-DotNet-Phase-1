a29d9dac986c605074eb314138955653bd8d3359
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2025-01-29 15:24:57 +0530

    Added new ClassMethodStaticDemo project files

---
+++ Day3/ClassMethodStaticDemo/EmployeeCRUDOp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodStaticDemo
+{
+    internal class EmployeeCRUDOp
+    {
+        private const int size = 5;
+        //create an array of 5 employees
+        int[] ar = new int[5];
+        Employee[] emps = new Employee[size];
+        //Employee emp = new Employee();
+        private int Empcount = 0;
+
+        //Methods to perform CRUD
+        public void AddEmployee()
+        {
+            //emps[Empcount] = new Employee(EmpId, EmpName, Dept, Salary);
+        }
+        public void ListEmployee()
+        {
+
+        }
+        public void UpdateEmployee()
+        {
+
+        }
+        public void DeleteEmployee()
+        {
+
+        }
+    }
+}
---
+++ Day3/ClassMethodStaticDemo/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace ClassMethodStaticDemo
+{
+    internal class Program
+    {
+        static void Display(ref int n)//Method Definition
+        {
+            n *= 20; // 400
+            Console.WriteLine("Value inside the method:" +n);
+        }
+
+        static void Outsample(out int n)//Method Definition
+        {
+            //n *= 20; 
+            int a = 20;
+            n = a * 20;
+            Console.WriteLine("Value inside the outsample method:" + n);
+        }
+
+        static int Division(int a, int b, out int r)
+        {
+            r=a%b;//remainder will be returned as out parameter
+            return a / b; //quotient
+        }
+
+        void Show() //Instance Method
+        {
+            Console.WriteLine("Instance Method");
+        }
+        //Method with return type
+        int add(int a, int b)
+        {
+            return a + b;
+        }
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            program.Show();
+            int a = 20;
+            Console.WriteLine("The value of a: " + a);
+            Display(ref a);//method calling
+            Console.WriteLine("The value of a after calling display method: " + a);
+            int res = program.add(60, 50);
+            Console.WriteLine("Sum is :" + res);
+            Console.WriteLine(program.add(10, 20));
+
+            //Out parameter
+            Outsample(out a);
+            Console.WriteLine("The value of a after calling outsample method: " + a);
+            int result;
+            Console.WriteLine(Division(100,5,out result));
+            Console.WriteLine(result);
+
+
+            Employee[] emp = new Employee[2];
+            for (int i = 0; i < emp.Length; i++)
+            {
+                Console.WriteLine("Enter EmployeeId : ");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter EmployeeName : ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter Dept : ");
+                string dept = Console.ReadLine();
+                Console.WriteLine("Enter Salary : ");
+                decimal sal = Convert.ToDecimal(Console.ReadLine());
+                emp[i] = new Employee(id, name, dept, sal);
+            }
+            for (int i = 0; i < emp.Length; i++)
+            {
+                Console.WriteLine(emp[i].EmpId + " " + emp[i].EmpName);
+            }
+        }
+    }
+}
