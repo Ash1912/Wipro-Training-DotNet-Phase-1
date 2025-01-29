9ea3573ee1144c244ba9d2081a2676e60f02d9b4
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2025-01-29 10:45:51 +0530

    Day1

---
+++ Day1/WiproDemoDay1/WiproDemoDay1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;//namespace
+
+namespace WiproDemoDay1
+{
+    internal class Program
+    {
+        static void Main(string[] args) //Entry point of ur code
+        {
+            Console.WriteLine("Enter the training name : ");
+            string name = Console.ReadLine(); //Read Input from the user 
+            //Console.WriteLine("Training name :" +name);
+
+            Console.WriteLine("Enter the organization name :");
+            string OrgName = Console.ReadLine();
+
+            Console.WriteLine("No. of days of training : ");
+            int num_days = Convert.ToInt32(Console.ReadLine()); // will not throwexception for null value, it simply returns 0
+            //int num_of_days =int.Parse(Console.ReadLine()); // will throw null
+
+            //Output Formatting
+            Console.WriteLine("Training name : " + name + " by " + OrgName);
+
+            //String Interpolation
+            Console.WriteLine($"Training name : {name}\n Organization name : {OrgName}\n Number of Days : {num_days}");
+
+            //Index Positions
+            Console.WriteLine("Training name : {0} and Organization name : {1}", name, OrgName);
+
+            int? a = null; //Variable a is of nullable type
+            int b = a ?? 10; //Null coleascing operator
+            Console.WriteLine("Value is : " + a + " "+b);
+
+            int d = 100;//value type
+            object ob = d;//reference type boxing - assigning value type to reference type
+            int unbox = (int)ob; // value type unboxing - reference type to value type
+            Console.WriteLine(d + " " +ob);
+        }
+    }
+}
---
+++ Day1/WiproDemoTryParseDay1/WiproDemoTryParseDay1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace WiproDemoTryParseDay1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //string input = "123";
+            string input = null;
+
+            int res;
+            bool isParse = int.TryParse(input, out res);
+            if (isParse)
+            {
+                Console.WriteLine(res);
+            }
+            else
+            {
+                Console.WriteLine("Parse Failed");
+            }
+            int resConvert = Convert.ToInt32(input);
+            if (resConvert != 0)
+            {
+                Console.WriteLine(resConvert);
+            }
+            else
+            {
+                Console.WriteLine("Convert.ToInt32 Failed");
+            }
+            int age = GetValidInteger("Enter your current age : ");
+            int birthYear = GetValidInteger("Enter your birth year : ");
+            Console.WriteLine($"You entered:\nAge: {age}\nBirth Year: {birthYear}");
+        }
+        static int GetValidInteger(string prompt)
+        {
+            int result;
+            while (true)
+            {
+                Console.Write(prompt);
+                if (int.TryParse(Console.ReadLine(), out result))
+                {
+                    return result;
+                }
+                Console.WriteLine("Invalid Input");
+                return 0;
+            }
+        }
+    }
+}
