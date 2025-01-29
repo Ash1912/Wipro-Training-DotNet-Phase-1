5faf7cb3a62ac7448d6b511c308e6234ed21910c
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2025-01-29 15:26:03 +0530

    Modified project files in AccessSpecifierdemo and WiproAccessModifierDay3

--- Day3/AccessSpecifierdemo/obj/Debug/net8.0/AccessSpecifierdemo.AssemblyInfo.cs
+++ Day3/AccessSpecifierdemo/obj/Debug/net8.0/AccessSpecifierdemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccessSpecifierdemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acbd19ee8e1b09c2ea017dfb3e6a9149b4d44d9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b330b7ec25be01fe07cc85a0332380b652b2242")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccessSpecifierdemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccessSpecifierdemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
