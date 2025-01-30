9cfed707c01e65b095e97b75c696a28509437ee1
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2025-01-30 15:14:37 +0530

    GetterSetter, MethodOverload, Base Class Constructor Added

---
+++ Day4/GetterSetterMethodDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace GetterSetterMethodDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Stuid = 19;
+            student.Name = "Ashish";
+            student.Age = 25;
+            Console.WriteLine(student.Stuid);
+            Console.WriteLine(student.Name);
+            Console.WriteLine(student.Age);
+            //student.SId = 111;//set accessor will invoke
+            //Console.WriteLine(student.SId);//get access will invoke
+        }
+    }
+}
---
+++ Day4/MethodOverloadingDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace MethodOverloadingDemo
+{
+    class AddOperat
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void Add(int a, int b, int c)
+        {
+            Console.WriteLine(a + b);
+        }
+        public string Add(string a, string b)
+        {
+            return a + b;
+        }
+        public void Add(double c, float d)
+        {
+            Console.WriteLine(c + d);
+        }
+    }
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            AddOperat addOperat = new AddOperat();
+            Console.WriteLine(addOperat.Add("Csharp", "training"));
+            AddOperat.Add(10,20,30);
+        }
+    }
+}
