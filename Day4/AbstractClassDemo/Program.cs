6ff7ece6b0128439c6e403afbbbb71099270ceb0
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2025-01-30 15:39:41 +0530

    AbstractClassDemo Added

---
+++ Day4/AbstractClassDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AbstractClassDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Abstract class cannot be instantiated
+            //It is not mandatory to have abstract method in abstract class.
+            //Generally,we go for abstract in order to show essential features and hide method implementation
+            //Flight flight = new Flight();
+            FlightFare flightFare = new FlightFare() { FlightNo = 11, FlightName = "AirIndia", durationinhrs = 6, typedes = "International"};
+            flightFare.FlightDetails();
+            flightFare.FareDetails();
+
+            //create instance for FlightFare by making as a reference to Flight which is abstract class
+            Flight ff = new FlightFare() { FlightNo = 12, FlightName = "Spicejet", durationinhrs = 3, typedes = "National" };
+            ff.FlightDetails();
+            ff.FareDetails();
+        }
+    }
+}
