7638c4dd125679ddf3449f55dfd413ae757c82d6
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2025-02-03 09:53:46 +0530

    PartialClasses

---
+++ Day6/PartialClasses/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.Metrics;
+using PartialClasses;
+
+Console.WriteLine("Hello, World!");
+
+Country obj = new Country();
+obj.CountryMethod();
+obj.ParentCountryMethod();
