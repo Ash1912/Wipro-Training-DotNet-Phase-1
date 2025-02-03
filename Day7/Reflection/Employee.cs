3e6ad6fc10f8a30371aaf1291d2b79a42a39416d
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2025-02-03 16:31:21 +0530

    Reflection

---
+++ Day7/Reflection/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public Employee()
+        {
+            Console.WriteLine("Default constructor");
+        }
+        public Employee(int ID, string Name, string Address)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                throw new ArgumentException("Name cannot be null or empty", nameof(Name));
+            }
+
+            this.Id = ID;
+            this.Name = Name;  // Ensure Name is never null or empty
+            this.Address = Address;
+        }
+
+
+        public void PrintID()
+        {
+            Console.WriteLine("Display" + Id);
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine("Name" + Name);
+        }
+
+    }
+}
