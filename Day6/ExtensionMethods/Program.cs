868f3c638102e0b6324a9d24cf8865e56534c6ae
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2025-02-03 09:47:46 +0530

    Day6/ExtensionMethods/

---
+++ Day6/ExtensionMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using ExtensionMethods;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 15;
+        Console.WriteLine(num.IsEvenOrOdd());
+
+        int? a = 10;
+        int b = a ?? 5;
+
+    }
+}
