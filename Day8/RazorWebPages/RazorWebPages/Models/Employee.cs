0083edc94445f7dd31ae07776ac6758e59ee9cc8
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2025-02-05 22:46:01 +0530

    RazorWebPages

---
+++ Day8/RazorWebPages/RazorWebPages/Models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace WiproRazorWebPagesDemo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Day8/RazorWebPages/RazorWebPages/Pages/Employee.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiproRazorWebPagesDemo.Models;
+
+namespace WiproRazorWebPagesDemo.Pages
+{
+    public class EmployeeModel : PageModel
+    {
+        [BindProperty]
+        public Employee emps { get; set; } = new Employee();
+        public void OnGet()
+        {
+            //This method will be called when the page is loaded
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return Page();
+        }
+    }
+}
