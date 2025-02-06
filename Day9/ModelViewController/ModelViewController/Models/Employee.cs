71595aaa6a83cef7c03da9d85af1e4d4fe8164c2
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2025-02-06 21:03:38 +0530

    MVCCRUD

---
+++ Day9/ModelViewController/ModelViewController/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelViewController.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Day9/ModelViewController/ModelViewController/Models/EmployeeDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ModelViewController.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; } 
+
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options) { }
+    }
+}
