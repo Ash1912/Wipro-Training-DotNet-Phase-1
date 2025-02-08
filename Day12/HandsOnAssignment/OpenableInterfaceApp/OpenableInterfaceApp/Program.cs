2f94ad580c1c8b638cc014a29562bb5b3a23d54f
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2025-02-08 17:35:22 +0530

    Assignment and OpenableInterfaceApp

---
+++ Day12/HandsOnAssignment/OpenableInterfaceApp/OpenableInterfaceApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenableInterfaceApp;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the letter found in the paper");
+        string input = Console.ReadLine()?.Trim().ToUpper();
+
+        IOpenable item;
+
+        if (input == "T")
+        {
+            item = new TreasureBox();
+        }
+        else if (input == "P")
+        {
+            item = new Parachute();
+        }
+        else
+        {
+            Console.WriteLine("Invalid input! Please enter 'T' or 'P'.");
+            return;
+        }
+
+        Console.WriteLine(item.OpenSesame());
+    }
+}
---
+++ Day12/HandsOnAssignment/OpenableInterfaceApp/OpenableInterfaceApp/TreasureBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenableInterfaceApp
+{
+    public class TreasureBox : IOpenable
+    {
+        public string OpenSesame()
+        {
+            return "Congratulations, Here is your lucky win";
+        }
+    }
+
+}
