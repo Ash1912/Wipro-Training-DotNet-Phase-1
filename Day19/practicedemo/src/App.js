956584c1a8ca0b4c1a8ca14153ee7a461ee1d8bd
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2025-02-18 11:17:54 +0530

    Practice

---
+++ Day19/practicedemo/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import App1 from './App1';
+import App2 from './App2';
+import App3 from './App3';
+import App4 from './App4';
+import Parent from './Parent';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <App1/>
+      <App2/>
+      <App3/> */}
+      <App4/>
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
